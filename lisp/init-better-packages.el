3b8fc457f9746ceb197170a657352375f0d9c1e3
YanTree <yanshudream@outlook.com>
2019-10-14 13:18:14 +0800

    配置 ediff

--- lisp/init-better-packages.el
+++ lisp/init-better-packages.el
@@ -35,6 +35,20 @@
 
 
 ;;----------------------------------------------------------------------------
+;; ediff (A comprehensive visual interface to diff & patch)
+(use-package ediff
+  :ensure nil
+  :hook(;; show org ediffs unfolded
+        (ediff-prepare-buffer . outline-show-all)
+        ;; restore window layout when done
+        (ediff-quit . winner-undo))
+  :config
+  (setq ediff-window-setup-function 'ediff-setup-windows-plain)
+  (setq ediff-split-window-function 'split-window-horizontally)
+  (setq ediff-merge-split-window-function 'split-window-horizontally))
+
+
+;;----------------------------------------------------------------------------
 ;; flyspell (需要安装 aspell 和 aspell-en)
 (use-package flyspell
   :ensure nil
